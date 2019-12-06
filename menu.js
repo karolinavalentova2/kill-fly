fb29d4c14a96ffefd0e4e9e19a2b23c6cb24c469
petra2301 <hello@petragergely.dk>
2019-12-06 17:34:02 +0100

    signup form functional with POST

---
+++ menu.js
@@ -0,0 +1,12 @@
+const menu = document.querySelector(".menu");
+
+document.querySelector("#mobileMenu").addEventListener("click", displayMenu);
+
+function displayMenu() {
+  if (menu.style.display = "none") {
+  menu.style.display = "flex"; document.querySelector("#mobileMenu").style.background="url('http://petragergely.dk/kea/imgs/delete.png')";  
+    document.querySelector("#mobileMenu").style.backgroundSize = "cover";
+  } else {
+  menu.style.display = "none"; document.querySelector("#mobileMenu").style.background="url('http://petragergely.dk/kea/imgs/menu.png')"; 
+  }
+}
