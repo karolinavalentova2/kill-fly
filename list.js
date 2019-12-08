9b6492c926b34e1c1b6da5ca2175adeeae6659c0
karolinakv <karolinavalentova2@gmail.com>
2019-12-08 21:33:56 +0100

    added sorting

--- list.js
+++ list.js
@@ -1,13 +1,15 @@
 "use strict";
 
-let entries;
+let entries = null;
 let SVGS = {};
-let userStatus = false;
-
-loadSVG().then().catch();
+let SortBY = {
+    lastSortingOrderItem: null,
+    lastSortingMethod: 'ASC',
+};
 
 async function doStart() {
     try {
+        await loadSVG();
         doSetupIcons();
         getAllEntries();
     } catch (e) {
@@ -29,12 +31,76 @@ async function loadSVG() {
 }
 
 function doSetupIcons() {
-    document.getElementById('nameArrowSVG').innerHTML = SVGS.arrowBTN;
+    // document.getElementById('nameArrowSVG').innerHTML = SVGS.arrowBTN;
     document.getElementById('tableRefreshSVG').innerHTML = SVGS.refreshBTN;
 
     const arrowSVGS = document.getElementsByClassName('table-arrow-svg');
-    Array.from(arrowSVGS).forEach(element =>
-        element.innerHTML = SVGS.arrowBTN);
+    Array.from(arrowSVGS).forEach((element, index) =>
+    {
+        element.innerHTML = SVGS.arrowBTN;
+
+        element.children[0].onclick = () => {
+            sortBy(element.children[0], index)
+        };
+
+        element.children[0].setAttribute('sorting-order', 'ASC');
+        element.children[0].id = String(index);
+    });
+}
+
+function sortBy(arrowSVG, typeOfSort) {
+    if(!entries) return;
+
+    arrowSVG.setAttribute('sorting-order', (arrowSVG.getAttribute('sorting-order') === 'ASC') ? 'DESC' : 'ASC');
+    arrowSVG.classList.toggle('desc');
+
+    const currentSortOrder = arrowSVG.getAttribute('sorting-order');
+
+    if(SortBY.lastSortingOrderItem && SortBY.lastSortingOrderItem.id !== arrowSVG.id) {
+        SortBY.lastSortingOrderItem.classList.remove('desc');
+        SortBY.lastSortingOrderItem.setAttribute('sorting-oder', 'ASC');
+    }
+
+    let sortBY;
+    switch (typeOfSort) {
+        case 0: { // Sort by name
+            sortBY = 'name';
+            break;
+        }
+        case 1: { // Sort by name
+            sortBY = 'email';
+            break;
+        }
+        case 2: { // Sort by name
+            sortBY = 'id';
+            break;
+        }
+        case 3: { // Sort by name
+            sortBY = 'score';
+            break;
+        }
+        default: {
+            sortBY = null;
+            break;
+        }
+    }
+
+    if(entries.length > 0) {
+        const tmpSortedEntries = entries.sort((elemA, elemB) => {
+            if(currentSortOrder === 'DESC') {
+                if(elemA[sortBY] < elemB[sortBY]) return 1;
+                else return -1;
+            } else if(currentSortOrder === 'ASC') {
+                if(elemA[sortBY] > elemB[sortBY]) return 1;
+                else return -1;
+            }
+        });
+
+        doClearEntriesTable();
+        doReloadAllEntries(tmpSortedEntries);
+    }
+
+    SortBY.lastSortingOrderItem = arrowSVG;
 }
 
 function getAllEntries() {
@@ -51,6 +117,7 @@ function getAllEntries() {
         .then(e => {
             e.json().then((data) => {
                 if(data.length !== 0) {
+                    entries = [...data];
                     data.forEach((entry) => {
                         addNewEntryToHTML(entry)
                     });
@@ -63,6 +130,25 @@ function getAllEntries() {
         });
 }
 
+function updateUser(userInfo) {
+    fetch(`https://smackfly-2fd1.restdb.io/rest/users-fly-smacker/${userInfo.id}`, {
+        method: "put",
+        headers: {
+            "Content-Type": "application/json; charset=utf-8",
+            "x-apikey": "5de40f274658275ac9dc2152",
+            "cache-control": "no-cache",
+            // "Access-Control-Allow-Origin": "*",
+        },
+        body: userInfo,
+        //mode: "no-cors",
+    })
+        .then(e => {
+            console.log('User updated')
+        })
+        .catch(e => {
+            console.log(e);
+        });
+}
 // function doAnimateDeleteButton(button) {
 //     button.children[2].classList.add('animate');
 //     setTimeout(() => {
@@ -72,17 +158,29 @@ function getAllEntries() {
 //     //TODO: Change the button style to deleted by adding some class to it (smoothen with transition)
 //     //TODO: This should act as a trigger, must be aware of the position of the button as well (delete/restore)
 // }
+function doToggleUserState(userInfo, checkbox) {
+    checkbox.checked = !!checkbox.checked;
+    userInfo.edit = !!checkbox.checked;
 
+    checkbox.parentElement.children[2].textContent = checkbox.checked ? 'Disabled' : 'Active';
+
+    updateUser(userInfo);
+}
 function addNewEntryToHTML(entry) {
     const entryTemplate = document.getElementById('entryTemplate');
     const newEntryTemplate = entryTemplate.content.cloneNode(true);
-
+    const toggle = newEntryTemplate.firstElementChild.children[4].children[0].children[0];
     newEntryTemplate.firstElementChild.children[0].textContent = entry.name;
     newEntryTemplate.firstElementChild.children[1].textContent = entry.email;
     newEntryTemplate.firstElementChild.children[2].textContent = entry.id;
     newEntryTemplate.firstElementChild.children[3].textContent = entry.score;
-    newEntryTemplate.firstElementChild.children[4].children[0].children[2].innerHTML = entry.edit;
+    toggle.checked = entry.edit;
 
+    toggle.onclick = () => {
+        doToggleUserState(entry, toggle)
+    };
+
+    newEntryTemplate.firstElementChild.children[4].children[0].children[2].textContent = entry.edit ? 'Disabled' : 'Active';
 
     // const deleteButton = newEntryTemplate.firstElementChild.children[4];
     // deleteButton.innerHTML = `<span class="user-status"><span>Active</span>${SVGS.toggleSVG}</span>`;
@@ -101,4 +199,16 @@ function addNewEntryToHTML(entry) {
     document.getElementById('entriesContainer').appendChild(newEntryTemplate);
 }
 
+function doClearEntriesTable() {
+    const parent = document.getElementById('entriesContainer');
+    while (parent.firstChild) {
+        parent.firstChild.remove();
+    }
+}
+
+function doReloadAllEntries(newArray) {
+    if(newArray.length) {
+        newArray.forEach(entry => addNewEntryToHTML(entry));
+    }
+}
 document.body.onload = doStart;
