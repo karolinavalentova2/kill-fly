343d4c88544c7992f8778ae976e1afb3f4c35749
karolinakv <karolinavalentova2@gmail.com>
2019-12-17 05:22:25 +0100

    added wp

--- store.js
+++ store.js
@@ -1,3 +1,4 @@
+'use strict';
 const menu = document.querySelector(".menu");
 
 document.querySelector("#mobileMenu").addEventListener("click", displayMenu);
@@ -11,3 +12,55 @@ function displayMenu() {
     }
 }
 
+window.onload = () => {
+    doGetShopEntries();
+};
+
+function doGetShopEntries() {
+    axios.get('http://karolinavalentova.me/wp/?rest_route=/wp/v2/killfly-store')
+        .then(function (response) {
+            if(response.status === 200 && response.data.length) {
+                doAddEntriesToTheShop(response.data);
+            }
+        })
+        .catch(function (error) {
+            console.log(error);
+        })
+}
+
+function doAddEntriesToTheShop(entries) {
+    const container = document.getElementById('productsContainer');
+    entries.forEach((entry) => {
+        const template = document.getElementById('shopEntry');
+        const entryElement = template.content.cloneNode(true);
+        const { img, name_item, description, price } = entry;
+
+        entryElement.children[0].children[0].src = img.guid;
+        entryElement.children[0].children[1].textContent = name_item;
+        entryElement.children[0].children[2].textContent = description;
+        entryElement.children[0].children[3].children[0].textContent = `${price}$`;
+        entryElement.children[0].children[3].children[1].onclick = () => {
+            doAddItemToCart({
+                img,
+                name_item,
+                price
+            })
+        };
+
+        container.appendChild(entryElement);
+    })
+}
+
+function doAddItemToCart(item) {
+    let currentBasket = localStorage.getItem('CURRENT_ITEMS_IN_BASKET');
+    if(currentBasket) {
+        currentBasket = JSON.parse(currentBasket);
+        currentBasket.push(item);
+
+        localStorage.setItem('CURRENT_ITEMS_IN_BASKET', JSON.stringify(currentBasket));
+    } else {
+        const newBasket = [];
+        newBasket.push(item);
+        localStorage.setItem('CURRENT_ITEMS_IN_BASKET', JSON.stringify(newBasket));
+    }
+}
